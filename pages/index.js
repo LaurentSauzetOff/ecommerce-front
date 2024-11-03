863133c1b6650058795b59444d13ef973935fdc3
SAUZET Laurent <laurent.sauzetpro@gmail.com>
2024-11-03 19:52:16 +0100

    Latest products

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,32 @@
 import Header from "@/components/Header";
 import Featured from "@/components/Featured";
-import {Product} from "@/models/Product";
-import {mongooseConnect} from "@/lib/mongoose";
-/* import NewProducts from "@/components/NewProducts"; */
+import { Product } from "@/models/Product";
+import { mongooseConnect } from "@/lib/mongoose";
+import NewProducts from "@/components/NewProducts";
 
-export default function HomePage({featuredProduct,newProducts}) {
-  console.log("featuredProduct : ", featuredProduct)
+export default function HomePage({ featuredProduct, newProducts }) {
+  console.log("NewProducts : ", NewProducts);
   return (
     <div>
       <Header />
       <Featured product={featuredProduct} />
-     {/*  <NewProducts products={newProducts} /> */}
+      <NewProducts products={newProducts} />
     </div>
   );
 }
 
 export async function getServerSideProps() {
-  const featuredProductId = '6727b8a5ea31669b395d0166';
+  const featuredProductId = "6727b8a5ea31669b395d0166";
   await mongooseConnect();
   const featuredProduct = await Product.findById(featuredProductId);
- /*  const newProducts = await Product.find({}, null, {sort: {'_id':-1}, limit:10}); */
+  const newProducts = await Product.find({}, null, {
+    sort: { _id: -1 },
+    limit: 10,
+  });
   return {
     props: {
       featuredProduct: JSON.parse(JSON.stringify(featuredProduct)),
-      /* newProducts: JSON.parse(JSON.stringify(newProducts)), */
+      newProducts: JSON.parse(JSON.stringify(newProducts)),
     },
   };
 }
-
-
-
-
