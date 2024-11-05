506a79ad8fd6597213375a5c55006ad054d776ed
SAUZET Laurent <laurent.sauzetpro@gmail.com>
2024-11-05 22:25:49 +0100

    first steps of stripe webhook

--- components/CartContext.js
+++ components/CartContext.js
@@ -4,43 +4,32 @@ export const CartContext = createContext({});
 
 export function CartContextProvider({ children }) {
   const ls = typeof window !== "undefined" ? window.localStorage : null;
-
   const [cartProducts, setCartProducts] = useState([]);
-
-  const clearCart = () => {
-    setCartProducts([]);
-  };
-
   useEffect(() => {
     if (cartProducts?.length > 0) {
       ls?.setItem("cart", JSON.stringify(cartProducts));
     }
   }, [cartProducts]);
-
   useEffect(() => {
-    if (ls) {
-      const storedCart = ls.getItem("cart");
-      if (storedCart) {
-        setCartProducts(JSON.parse(storedCart));
-      } else {
-        setCartProducts([]);
-      }
+    if (ls && ls.getItem("cart")) {
+      setCartProducts(JSON.parse(ls.getItem("cart")));
     }
   }, []);
   function addProduct(productId) {
     setCartProducts((prev) => [...prev, productId]);
   }
-
   function removeProduct(productId) {
-    setCartProducts(prev => {
+    setCartProducts((prev) => {
       const pos = prev.indexOf(productId);
       if (pos !== -1) {
         return prev.filter((value, index) => index !== pos);
       }
       return prev;
-    })
+    });
   }
-
+  function clearCart() {
+    setCartProducts([]);
+  }
   return (
     <CartContext.Provider
       value={{
@@ -48,7 +37,7 @@ export function CartContextProvider({ children }) {
         setCartProducts,
         addProduct,
         removeProduct,
-        clearCart
+        clearCart,
       }}
     >
       {children}
