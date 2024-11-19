e290cb453ab4c618a364d2d4ca17dd253d66c239
SAUZET Laurent <laurent.sauzetpro@gmail.com>
2024-11-19 16:04:19 +0100

    First commit with account page

---
+++ pages/account.js
@@ -0,0 +1,11 @@
+import Header from "@/components/Header";
+import Title from "@/components/Title";
+
+export default function AccountPage() {
+  return (
+    <>
+      <Header />
+      <Title>Account</Title>
+    </>
+  );
+}
